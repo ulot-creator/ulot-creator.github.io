1ce710c9540ad5c6cf0260d3d296102333c71ba5
ulot-creator <ephraimorimolade608@gmail.com>
2022-03-17 08:35:39 +0100

    test

---
+++ trendz- darkmode/app.js
@@ -0,0 +1,9 @@
+const a = 5;
+const b = 6;
+const c = a + b;
+console.log(c);
+ fuction dis(){
+   document.getElementById('')
+
+
+ }
---
+++ trendz- darkmode/test.py
@@ -0,0 +1,5 @@
+a = "Tolu"
+b = "transexual non-binary gayman"
+
+c = "My name is " + a + " and I am a " + b
+print(c)
